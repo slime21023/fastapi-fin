48a263dad0a0811678b5e676fa29c0315655ed02
kevinchueh <kevinchueh@retailingdata.com.tw>
2025-03-05 15:34:19 +0800

    init

---
+++ fastapi_fin/middleware.py
@@ -0,0 +1,66 @@
+from lilya.protocols.middleware import MiddlewareProtocol
+
+
+# TODO : implement Basic Authentication
+class BasicAuthMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO : implement Bearer Authentication
+class BearerAuthMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO : implement Cache Middleware
+class CacheMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO : implement Compress Middleware
+class CompressMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO : implement CORS Middleware
+class CORSMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement CSRF Protection Middleware
+class CSRFProtectionMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement ETag Middleware
+class ETagMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement IP Restriction Middleware
+class IPRestrictionMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement JWK Middleware
+class JWKMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement JWT Middleware
+class JWTMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement Secure Headers Middleware
+class SecureHeadersMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement Timeout Middleware
+class TimeoutMiddleware(MiddlewareProtocol):
+    pass
+
+
+# TODO: implement Body Limit Middleware
+class BodyLimitMiddleware(MiddlewareProtocol):
+    pass
